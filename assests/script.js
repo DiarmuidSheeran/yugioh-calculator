36b36b3b4e5ebfda343f18da41515b4e2ef1d5cc
Diarmuid Sheeran <dssheeran@outlook.com>
2023-05-06 16:41:07 +0000

    create to functions to prompt user to enter names, pass names to variable and change value of in put field to name

--- assests/script.js
+++ assests/script.js
@@ -1,3 +1,16 @@
+//Player names stored in these variables
 let playerName1 = document.getElementById("player-1");
 let playerName2 = document.getElementById("player-2");
-    
+
+//prompt user to enter a name when text field is clicked. Name is stored on the console and the value is passed to variabele playerName1
+function submitName1() {         
+    let enterName1 = prompt("Enter Player Ones Name: ") 
+    console.log("Player One: " + enterName1)
+    playerName1.value = enterName1
+}
+//prompt user to enter a name when text field is clicked. Name is stored on the console and the value is passed to variabele playerName2
+function submitName2() {
+    let enterName2 = prompt("Enter Player Ones Name: ") 
+    console.log("Player One: " + enterName2)
+    playerName2.value = enterName2
+}
